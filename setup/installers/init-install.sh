4510e099d2f64143c1753a31b26fed130df91b8e
Patrick Carlo-Hickman <patrick.carlohickman@gmail.com>
2023-01-23 01:06:29 -0500

    Add comment to init scripts to explain duplicate functions.

--- setup/installers/init-install.sh
+++ setup/installers/init-install.sh
@@ -12,6 +12,10 @@ function main {
   fi
 }
 
+# This script uses the following functions. If the .env and common.sh scripts
+# are sourced properly, these functions will be redefined. However, if there
+# is an issue, these functions need to be defined here to display the error.
+
 function script_name {
   echo "$(basename "${0}")"
 }
@@ -28,14 +32,14 @@ function log {
   local -r MSG="${1}"
   local -r LEVEL="${2:-INFO}"
   local -r FD=${3:-1}
-  
+
   echo -e "[$(now)] [$(script_name)] [${LEVEL}] : ${MSG}" >&${FD}
 }
 
 function log_error {
   local -r COLOR_OFF=${COLOR_OFF:-"\e[0m"}
   local -r COLOR_RED=${COLOR_RED:-"\e[1;91m"}
-  
+
   log "${COLOR_RED}$@${COLOR_OFF}" "ERROR" 2
 }
 
