64161b1e5f284b16f305fd664d9fc1179df4aeeb
Patrick Carlo-Hickman <patrick.carlohickman@gmail.com>
2023-02-19 15:27:17 -0500

    Update the shebang to include the login flag for scripts that need PHPENV.

--- setup/installers/install-php.sh
+++ setup/installers/install-php.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 source "$(dirname "$(readlink -f "${0}")")/init-install.sh"
 
--- setup/resources/apache/make-site.sh
+++ setup/resources/apache/make-site.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 source "$(dirname "$(dirname "$(readlink -f "${0}")")")/init-resource.sh"
 
